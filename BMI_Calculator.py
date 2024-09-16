fec86baefc50135c955bcdac04ea606db190922e
csathishkumar <csathishkumar2002@gmail.com>
2024-09-16 15:35:30 +0530

    BMI Calculator

---
+++ BMI_Calculator.py
@@ -0,0 +1,4 @@
+m = float(input("enter the height:    "))
+kg = float(input("Enter the weight in kg:   "))
+
+print(kg/(m*m))
---
+++ YourLifeinWeeks.py
@@ -0,0 +1,13 @@
+# How many days can we live if we ;ive until 90 years old?
+
+age = input("What is your current age:  ")
+age = int(age)
+
+yearsRemains = 90 - age
+daysRemains = yearsRemains * 365
+weeksRemains = yearsRemains * 52
+monthsRemains = yearsRemains * 12
+
+message = f"You have {daysRemains} days, {weeksRemains} weeks, and {monthsRemains} are left!"
+print(message)
+
