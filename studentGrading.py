ad93990e8f6f19fc559cacbc50007863c9a801b0
csathishkumar <csathishkumar2002@gmail.com>
2024-09-17 15:23:01 +0530

    Student Grading with Python  Dictionary

---
+++ studentGrading.py
@@ -0,0 +1,17 @@
+#Dictionary 
+
+studentScores = {"Sathish":90,
+                 "Pradeep":95,
+                 "Ragul":85,
+                 "Ellappan":98,
+                 "Nandha":74,    
+                }
+for i in studentScores:
+    if 90>studentScores[i]<=100:
+        print(f"{i}'s Grade : OutStanding.")
+    if 80>studentScores[i]<=90:
+        print(f"{i}'s Grade : Exceed Expectation.")
+    if 70>studentScores[i]<=80:
+        print(f"{i}'s Grade : Acceptable.")        
+    if studentScores[i]<=70:
+        print(f"{i}'s Grade : Fail.")        
