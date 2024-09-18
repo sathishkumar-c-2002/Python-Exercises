859e119a53792e510feb8ea1e922fe581bab6c36
csathishkumar <csathishkumar2002@gmail.com>
2024-09-18 16:09:09 +0530

    Calculator

--- calculator.py
+++ calculator.py
@@ -12,10 +12,31 @@ operations = {
 }
 
 num1 = int(input("Enter Number 1 : "))
-op = input("Enter the operation : ")
-num2 = int(input("Enter Number 2 : "))
+choice = True
+while choice:
+    op = input("Enter the operation : ")
+    num2 = int(input("Enter Number 2 : "))
+    calculator = operations[op]
+    answer = calculator(num1,num2)
+    print(f"{num1} {op} {num2} = {answer}")
+    a = input("Do you want to continue Calculation? (Y/N)")
+    if a.lower()=="n":
+        choice = False
+    num1 = answer
 
-calculator = operations[op]
-answer = calculator(num1,num2)
-print(f"{num1} {op} {num2} = {answer}")
+# choice = True
+# while choice:
+#     num3 = int(input("Enter Number 3 : "))
+#     op = input("Enter the operation : ")
+#     calculator = operations[op]
+#     temp = answer
+#     answer = calculator(answer,num3)
+#     print(f"{temp} {op} {num3} = {answer}")
+#     a = input("Do you want to continue Calculation? (Y/N)")
+#     if a.lower()=="n":
+#         choice = False
+        
+    
+
+
     
